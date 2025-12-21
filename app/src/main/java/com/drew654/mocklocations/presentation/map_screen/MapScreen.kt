5e3f1b459dea9c824158637dc64eea5fffc40791
Drew Pusey <drewpusey654@gmail.com>
2025-12-21 15:18:22 -0600

    Fix dark mode

--- app/src/main/java/com/drew654/mocklocations/presentation/map_screen/MapScreen.kt
+++ app/src/main/java/com/drew654/mocklocations/presentation/map_screen/MapScreen.kt
@@ -3,6 +3,7 @@ package com.drew654.mocklocations.presentation.map_screen
 import android.Manifest
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import com.drew654.mocklocations.R
 import com.drew654.mocklocations.presentation.MockLocationsViewModel
 import com.drew654.mocklocations.presentation.map_screen.components.ControlButtons
 import com.google.android.gms.location.LocationServices
@@ -23,6 +25,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
@@ -36,6 +39,7 @@ fun MapScreen(
     viewModel: MockLocationsViewModel
 ) {
     val context = LocalContext.current
+    val systemInDarkTheme = isSystemInDarkTheme()
     val points by viewModel.points.collectAsState()
     var hasLocationPermission by remember { mutableStateOf(false) }
     val isMocking by viewModel.isMocking.collectAsState()
@@ -61,7 +65,11 @@ fun MapScreen(
     val mapProperties by remember(hasLocationPermission) {
         mutableStateOf(
             MapProperties(
-                isMyLocationEnabled = hasLocationPermission
+                isMyLocationEnabled = hasLocationPermission,
+                mapStyleOptions = MapStyleOptions.loadRawResourceStyle(
+                    context,
+                    if (systemInDarkTheme) R.raw.map_style_night else R.raw.map_style_standard
+                )
             )
         )
     }
