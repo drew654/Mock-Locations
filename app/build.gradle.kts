b3bce887dd1246598b7bc691435a66096e27945d
Drew Pusey <drewpusey654@gmail.com>
2025-12-23 06:52:44 -0600

    Set version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.drew654.mocklocations"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.0-alpha.1"
+        versionCode = 3
+        versionName = "0.1.0-alpha.2"
         setProperty("archivesBaseName", "mock-locations")
 
         val localProperties = Properties()
