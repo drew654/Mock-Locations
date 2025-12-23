62d6dcf81c2df70ecf2409b90b0fc327d2d12dd7
Drew Pusey <drewpusey654@gmail.com>
2025-12-22 22:39:27 -0600

    Set version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.1"
+        versionName = "0.1.0-alpha.1"
         setProperty("archivesBaseName", "mock-locations")
 
         val localProperties = Properties()
