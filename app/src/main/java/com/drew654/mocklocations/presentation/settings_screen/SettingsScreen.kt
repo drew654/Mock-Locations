891f83b44f2346ec957ca7aa0b6d879bfc9a4fc1
Drew Pusey <drewpusey654@gmail.com>
2026-01-18 01:50:14 -0600

    Settings screen window insets

--- app/src/main/java/com/drew654/mocklocations/presentation/settings_screen/SettingsScreen.kt
+++ app/src/main/java/com/drew654/mocklocations/presentation/settings_screen/SettingsScreen.kt
@@ -2,8 +2,11 @@ package com.drew654.mocklocations.presentation.settings_screen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -29,7 +32,13 @@ fun SettingsScreen(
     val clearPointsOnStop by viewModel.clearPointsOnStop.collectAsState()
 
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .windowInsetsPadding(
+                WindowInsets.displayCutout.only(
+                    androidx.compose.foundation.layout.WindowInsetsSides.Horizontal
+                )
+            ),
         topBar = {
             CenterAlignedTopAppBar(
                 title = { Text("Settings") },
