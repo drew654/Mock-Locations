982e5e7b5b9186e5cd5745a79628396b443e7eaf
Drew Pusey <drewpusey654@gmail.com>
2026-05-14 10:09:13 -0500

    Update tests

--- app/src/test/java/com/drew654/mocklocations/domain/model/MockControlActionTest.kt
+++ app/src/test/java/com/drew654/mocklocations/domain/model/MockControlActionTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 class MockControlActionTest {
     @Test
     fun `verify all enum constants exist`() {
-        val expectedActions = listOf("START", "STOP", "PAUSE", "RESUME", "ADD_POINT", "POP_POINT", "CLEAR_LOCATION_TARGET")
+        val expectedActions = listOf("START", "STOP", "PAUSE", "RESUME", "ADD_POINT", "LONG_PRESS_ADD_POINT", "POP_POINT", "CLEAR_LOCATION_TARGET")
 
         val actualActions = MockControlAction.entries.map { it.name }
 
