5b4791b9e8584cd54c51258a3da6dbd5665a3eaa
Drew Pusey <drewpusey654@gmail.com>
2026-05-18 20:14:20 -0500

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.drew654.mocklocations"
         minSdk = 31
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.4.0"
+        versionCode = 17
+        versionName = "0.4.1"
         setProperty("archivesBaseName", "mock-locations")
 
         val localProperties = Properties()
