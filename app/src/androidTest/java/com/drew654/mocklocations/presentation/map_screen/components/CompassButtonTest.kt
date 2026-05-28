ac6f2990b59de79ffabd7e2a42f3002ba2ba32b3
Drew Pusey <drewpusey654@gmail.com>
2026-05-28 09:26:25 -0500

    CompassButtonTest

---
+++ app/src/androidTest/java/com/drew654/mocklocations/presentation/map_screen/components/CompassButtonTest.kt
@@ -0,0 +1,28 @@
+package com.drew654.mocklocations.presentation.map_screen.components
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+
+class CompassButtonTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun clickButton_triggersCallback() {
+        var clicked = false
+        composeTestRule.setContent {
+            CompassButton(
+                onClick = { clicked = true },
+                bearing = { 0f }
+            )
+        }
+
+        composeTestRule.onNodeWithContentDescription("Align North").performClick()
+
+        assertTrue(clicked)
+    }
+}
