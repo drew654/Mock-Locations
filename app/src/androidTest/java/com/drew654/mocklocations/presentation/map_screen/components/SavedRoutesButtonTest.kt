a77d82497b6c0badf0cdac5c33b121fea98db2ef
Drew Pusey <drewpusey654@gmail.com>
2026-06-01 09:51:53 -0500

    SavedRoutesButtonTest

---
+++ app/src/androidTest/java/com/drew654/mocklocations/presentation/map_screen/components/SavedRoutesButtonTest.kt
@@ -0,0 +1,27 @@
+package com.drew654.mocklocations.presentation.map_screen.components
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+
+class SavedRoutesButtonTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun clickButton_triggersCallback() {
+        var clicked = false
+        composeTestRule.setContent {
+            SavedRoutesButton(
+                onClick = { clicked = true }
+            )
+        }
+
+        composeTestRule.onNodeWithContentDescription("Saved Routes").performClick()
+
+        assertTrue(clicked)
+    }
+}
