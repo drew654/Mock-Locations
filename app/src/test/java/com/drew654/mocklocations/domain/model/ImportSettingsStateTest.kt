1d96a86a7b70e3fd9922342982a67816c58640a2
Drew Pusey <drewpusey654@gmail.com>
2026-06-09 09:45:05 -0500

    More unit tests

---
+++ app/src/test/java/com/drew654/mocklocations/domain/model/ImportSettingsStateTest.kt
@@ -0,0 +1,63 @@
+package com.drew654.mocklocations.domain.model
+
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import org.junit.Test
+
+class ImportSettingsStateTest {
+    @Test
+    fun `data class properties are correctly initialized`() {
+        val isImportRoutesEnabled = true
+        val isImportRoutes = true
+        val isImportSettingsEnabled = true
+        val isImportSettings = true
+        val importRouteOption = ImportRouteOption.MERGE
+        val routesToImport = 5
+
+        val importSettingsState = ImportSettingsState(
+            isImportRoutesEnabled = isImportRoutesEnabled,
+            isImportRoutes = isImportRoutes,
+            isImportSettingsEnabled = isImportSettingsEnabled,
+            isImportSettings = isImportSettings,
+            importRouteOption = importRouteOption,
+            routesToImport = routesToImport
+        )
+
+        assertEquals(isImportRoutesEnabled, importSettingsState.isImportRoutesEnabled)
+        assertEquals(isImportRoutes, importSettingsState.isImportRoutes)
+        assertEquals(isImportSettingsEnabled, importSettingsState.isImportSettingsEnabled)
+        assertEquals(isImportSettings, importSettingsState.isImportSettings)
+        assertEquals(importRouteOption, importSettingsState.importRouteOption)
+        assertEquals(routesToImport, importSettingsState.routesToImport)
+    }
+
+    @Test
+    fun `isFormValid returns true when isImportSettings is true`() {
+        val importSettingsState = ImportSettingsState(
+            isImportSettingsEnabled = true,
+            isImportSettings = true
+        )
+
+        assertTrue(importSettingsState.isFormValid())
+    }
+
+    @Test
+    fun `isFormValid returns true when isImportRoutes is true`() {
+        val importSettingsState = ImportSettingsState(
+            isImportRoutesEnabled = true,
+            isImportRoutes = true,
+            importRouteOption = ImportRouteOption.MERGE,
+            routesToImport = 5
+        )
+
+        assertTrue(importSettingsState.isFormValid())
+    }
+
+    @Test
+    fun `isFormValid returns false when isImportRoutes and isImportSettings are false`() {
+        val importSettingsState = ImportSettingsState()
+
+        assertFalse(importSettingsState.isFormValid())
+    }
+}
--- app/src/test/java/com/drew654/mocklocations/domain/model/SavedCameraPositionTest.kt
+++ app/src/test/java/com/drew654/mocklocations/domain/model/SavedCameraPositionTest.kt
@@ -1,5 +1,6 @@
 package com.drew654.mocklocations.domain.model
 
+import com.google.android.gms.maps.model.LatLng
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -14,5 +15,17 @@ class SavedCameraPositionTest {
         assertEquals(latitude, savedCameraPosition.latitude, 0.0)
         assertEquals(longitude, savedCameraPosition.longitude, 0.0)
         assertEquals(zoom, savedCameraPosition.zoom, 0f)
+    }
+
+    @Test
+    fun `toLatLng returns correct values`() {
+        val savedCameraPosition = SavedCameraPosition(
+            latitude = 30.615165834851403,
+            longitude = -96.34165674448013,
+            zoom = 15f
+        )
+        val expected = LatLng(30.615165834851403, -96.34165674448013)
+
+        assertEquals(expected, savedCameraPosition.toLatLng())
     }
 }
